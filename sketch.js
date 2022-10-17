b25ccaf02d49265cb05466cb30725c2e58b97847
cindy920912 <115966010+cindy920912@users.noreply.github.com>
2022-10-17 11:38:44 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background(0);
+  rectMode(CENTER)//設定方塊座標點以中心點為座標
+  ellipse(25,25,50)//在座標為(x,y)產生一個50直徑的圓
+  noFill(0)//不充滿顏色
+
+  //fill//充滿顏色為
+  stroke(255)//框線顏色為白色
+  //整個指令使左上角產生一個白色"255"的圓(背景為黑色"0")
+
+  var w=mouseX/10
+  for(var j=0 ;j<height/50;j++)
+  {
+   {
+    for(var i=0 ;i<width/50;i++)
+   ellipse(25+50*i,25+50*j,w)
+   rect(25+50*i,25+50*j,mouseY)
+   ellipse(50+50*i,50+50*j,25)}
+
+   }
+}
